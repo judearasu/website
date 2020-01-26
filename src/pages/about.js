33766092fee4328678acb39e537b94019c6bcfc9
Jude Arasu <judearasu@gmail.com>
2020-01-25 23:07:04 -0500

    about page updated

--- src/pages/about.js
+++ src/pages/about.js
@@ -7,17 +7,17 @@ const AboutPage = () => (
   <Layout>
     <SEO title="About me" />
     <h1>About</h1>
-    <p className="caption text--subtitle">
+    <p>
       Hi there, I'm Thillai. I'm a software engineer currently working at a
       payroll company. I'm particulary interested in Golang, Data structures and
       Machine learning. I enjoy drawing UX paper prototypes for both my personal
       and professional projects.
     </p>
-    <p className="caption text--subtitle">
+    <p>
       Apart from computes, I really enjoy playing PUBG Mobile, traveling, hiking
       and watching NBA matches (LAKERS are my favaourites)
     </p>
-    <p className="caption text--subtitle">
+    <p>
       If you have any questions or just want to chat, you can reach out to me on
       <Link to="http://twitter.com/judearasu">Twitter</Link> or shoot me an{" "}
       <Link to="root@genlinux.org">email</Link>
